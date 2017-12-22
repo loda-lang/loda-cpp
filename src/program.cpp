2c4d13966c77a472f956ac54ac7c119eacb55e04
Christian Krause <christian.krause01@sap.com>
2017-12-22 13:17:35 +0100

    program cpp

---
+++ src/program.cpp
@@ -0,0 +1,7 @@
+#include "program.hpp"
+
+template<size_t NUM_VARS>
+bool Program<NUM_VARS>::isValid() const
+{
+  return true;
+}
--- src/program.hpp
+++ src/program.hpp
@@ -10,10 +10,14 @@ using var_t = std::size_t;
 template<size_t NUM_VARS>
 class Program
 {
+public:
+
   using Assignments = Assignment::Array<NUM_VARS>;
   using LoopVariables = std::vector<var_t>;
   using ProgramPtr = std::unique_ptr<Program<NUM_VARS> >;
   using Operation = std::tuple<Assignments, LoopVariables, ProgramPtr >;
+
+  bool isValid() const;
 
   std::vector<Operation> operations;
 };
