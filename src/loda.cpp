5134f2f9d96d2e6faca0550fdd67392dcf128fd6
Christian Krause <christian.krause01@sap.com>
2017-12-27 22:07:57 +0100

    simplified example

--- src/loda.cpp
+++ src/loda.cpp
@@ -5,46 +5,33 @@ int main( void )
 {
   Program p;
 
-  // n:0, one:1, u:2, v:3, w:4
+  // n:0, u:2, v:3, w:4
 
-  // set one,1
-  // cpy u,n
-  // cpy v,n
-  // sub u,one
+  // mov u,n
+  // mov v,n
+  // sub u,1
   // sub v,u
   // set w,1
   // lpb v
-  //   sub v,one
-  //   add w,one
+  //   sub v,1
+  //   sub w,1
   // lpe
 
   p.var_names[0] = "n";
-  p.var_names[1] = "one";
   p.var_names[2] = "u";
   p.var_names[3] = "v";
   p.var_names[4] = "w";
 
-  p.ops.resize( 10 );
-  p.ops[0].reset( new Mov( 1, Argument::Constant( 1 ) ) );
-  p.ops[1].reset( new Mov( 2, Argument::Variable( 0 ) ) );
-  p.ops[2].reset( new Mov( 3, Argument::Variable( 0 ) ) );
-  p.ops[3].reset( new Sub( 2, Argument::Variable( 1 ) ) );
-  p.ops[4].reset( new Sub( 3, Argument::Variable( 2 ) ) );
-  p.ops[5].reset( new Mov( 4, Argument::Constant( 1 ) ) );
-  p.ops[6].reset( new LoopBegin( { 3 } ) );
-  p.ops[7].reset( new Sub( 3, Argument::Variable( 1 ) ) );
-  p.ops[8].reset( new Sub( 4, Argument::Variable( 1 ) ) );
-  p.ops[9].reset( new LoopEnd() );
-
-  /*  p.ops[0].reset( new Set( 42, 0 ) );
-   p.ops[1].reset( new Set( 3, 1 ) );
-   p.ops[2].reset( new Set( 1, 2 ) );
-   p.ops[3].reset( new LoopBegin( { 1 } ) );
-   p.ops[4].reset( new Add( 0, 3 ) );
-   p.ops[5].reset( new Sub( 2, 1 ) );
-   p.ops[6].reset( new LoopEnd() );
-   p.ops[7].reset( new Add( 2, 3 ) );
-   */
+  p.ops.resize( 9 );
+  p.ops[0].reset( new Mov( 2, Argument::Variable( 0 ) ) );
+  p.ops[1].reset( new Mov( 3, Argument::Variable( 0 ) ) );
+  p.ops[2].reset( new Sub( 2, Argument::Constant( 1 ) ) );
+  p.ops[3].reset( new Sub( 3, Argument::Variable( 2 ) ) );
+  p.ops[4].reset( new Mov( 4, Argument::Constant( 1 ) ) );
+  p.ops[5].reset( new LoopBegin( { 3 } ) );
+  p.ops[6].reset( new Sub( 3, Argument::Constant( 1 ) ) );
+  p.ops[7].reset( new Sub( 4, Argument::Constant( 1 ) ) );
+  p.ops[8].reset( new LoopEnd() );
 
   try
   {
