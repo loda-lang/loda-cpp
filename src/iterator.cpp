789e0e48d1842b7bc91bd433e813a096a447e85e
Christian Krause <me@ckrause.org>
2018-09-02 16:01:40 +0200

    first half of program iterator

--- src/iterator.cpp
+++ src/iterator.cpp
@@ -1,7 +1,30 @@
 #include "iterator.hpp"
 
+inline bool inc( Operation& op )
+{
+  // TODO
+  return false;
+}
+
+inline void reset( Operation& op )
+{
+  op = Operation( Operation::Type::ADD, { Operand::Type::CONSTANT, 0 }, { Operand::Type::CONSTANT, 0 } );
+}
+
 Program Iterator::next()
 {
+  for ( auto& op : p_.ops )
+  {
+    if ( inc( op ) )
+    {
+      return p_;
+    }
+    else
+    {
+      reset( op );
+    }
+  }
+  p_.ops.push_back( Operation() );
+  reset( p_.ops.back() );
   return p_;
-
 }
