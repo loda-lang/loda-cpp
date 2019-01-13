0d240a921c8d184ad72c0ccc6d35286982ae645a
Christian Krause <me@ckrause.org>
2019-01-13 18:16:28 +0100

    optimizer improvements

--- mine_parallel.sh
+++ mine_parallel.sh
@@ -14,12 +14,12 @@ for n in 4 6 8; do
   ./loda mine -n ${n} -p ${n}0 -a cd -l ${log_level} &
 done
 
-for p in 10 20 30; do
+for p in 20 30 40; do
   ./loda mine -p ${p} -a cd -o asm -e programs/templates/T01.asm -l ${log_level} &
 done
 
-for p in 40 60 80 100; do
-  ./loda mine -n 6 -p ${p} -a cd -o asm -e programs/templates/T02.asm -l ${log_level} &
+for p in 20 30 40 50; do
+  ./loda mine -p ${p} -a cd -o asm -e programs/templates/T02.asm -l ${log_level} &
 done
 
 for p in 40 60; do
