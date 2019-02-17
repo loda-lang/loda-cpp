2921eb99868d5a4d3bf9461816546683ed722c84
Christian Krause <me@ckrause.org>
2019-02-17 12:50:29 +0100

    updated parallel mining scripts

--- mine_parallel.sh
+++ mine_parallel.sh
@@ -1,25 +1,60 @@
 #!/bin/bash
 
 log_level=alert
+restart_interval=3600
+min_changes=20
 
-trap abort INT
+function stop_miners() {
+  echo "Stopping miners"
+  killall loda > /dev/null
+}
 
-function abort() {
-  echo "Exiting..."
-  killall loda
+function abort_miners() {
+  stop_miners
   exit 1
 }
 
-for n in 2 4 6; do
-  p="${n}0"
-  l="-l ${log_level}"
-  ./loda mine -p $p -a cd -o asm -e programs/templates/T01.asm $l &
-  ./loda mine -p $p -a cd -o asm -e programs/templates/T02.asm $l &
-  ./loda mine -p $p -a cd -n $n $l &
-  ./loda mine -p $p -n $n $l &
-done
+function start_miners() {
+  echo "Updating OEIS database"
+  ./get_oeis.sh
+  ./make_charts.sh
+  echo "Start mining"
+  for n in 2 4 6; do
+    p="${n}0"
+    l="-l ${log_level}"
+    ./loda mine -p $p -a cd -o asm -e programs/templates/T01.asm $l &
+    ./loda mine -p $p -a cd -o asm -e programs/templates/T02.asm $l &
+    ./loda mine -p $p -a cd -n $n $l &
+    ./loda mine -p $p -n $n $l &
+  done
+  ./loda test &
+}
 
-input=
-while [ "$input" != "exit" ]; do
-  read input
+function push_updates {
+  if [ ! -d "programs" ]; then
+    echo "programs folder not found; aborting"
+    exit 1
+  fi
+  num_changes=$(git status programs -s | wc -l)
+  if [ "$num_changes" -ge "$min_changes" ]; then
+    echo "Pushing updates"
+    git pull
+    git add programs counts.png lengths.png README.md
+    git commit -m "updated $num_changes programs"
+    git push
+  fi
+}
+
+trap abort_miners INT
+
+start_miners
+SECONDS=0
+while [ true ]; do
+  if (( SECONDS >= restart_interval )); then
+  	stop_miners
+  	push_updates
+  	start_miners
+    SECONDS=0
+  fi
+  sleep 10
 done
