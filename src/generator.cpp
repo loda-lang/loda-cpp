fe46d621b1157e8919bd611108d602301badd16b
Christian Krause <me@ckrause.org>
2019-03-03 16:30:07 +0100

    Refactor distribution class

--- src/generator.cpp
+++ src/generator.cpp
@@ -7,9 +7,78 @@
 #include "printer.hpp"
 
 #include <algorithm>
+#include <iostream>
 #include <set>
+#include <stdexcept>
 
 #define POSITION_RANGE 100
+
+Distribution::Distribution( const std::vector<double>& probabilities )
+    : std::discrete_distribution<>( probabilities.begin(), probabilities.end() )
+{
+}
+
+Distribution Distribution::uniform( size_t size )
+{
+  return Distribution( std::vector<double>( size, 100.0 ) );
+}
+
+Distribution Distribution::exponential( size_t size )
+{
+  std::vector<double> probabilities( size );
+  double v = 1.0;
+  for ( int i = size - 1; i >= 0; --i )
+  {
+    probabilities[i] = v;
+    v *= 2.0;
+  }
+  return Distribution( probabilities );
+}
+
+Distribution Distribution::add( const Distribution& d1, const Distribution& d2 )
+{
+  auto p1 = d1.probabilities();
+  auto p2 = d2.probabilities();
+  if ( p1.size() != p2.size() )
+  {
+    throw std::runtime_error( "incompatibe distributions" );
+  }
+  std::vector<double> p( p1.size() );
+  for ( size_t i = 0; i < p.size(); i++ )
+  {
+    p[i] = (p1[i] + p2[i]) / 2;
+  }
+  return Distribution( p );
+}
+
+Distribution Distribution::mutate( const Distribution& d, std::mt19937& gen )
+{
+  std::vector<double> x;
+  x.resize( 100, 1 );
+  std::discrete_distribution<> v( x.begin(), x.end() );
+  auto p1 = d.probabilities();
+  std::vector<double> p2( p1.size() );
+  for ( size_t i = 0; i < p1.size(); i++ )
+  {
+    p2[i] = p1[i] + ((static_cast<double>( v( gen ) ) / x.size()) / 50);
+  }
+  return Distribution( p2 );
+}
+
+void Distribution::print() const
+{
+  auto probs = probabilities();
+  std::cout << "[";
+  for ( size_t i = 0; i < probs.size(); i++ )
+  {
+    if ( i > 0 )
+    {
+      std::cout << ",";
+    }
+    std::cout << probs[i];
+  }
+  std::cout << "]";
+}
 
 State::State()
     : State( 0, 4, 4, 2, 3 )
@@ -157,13 +226,13 @@ void Generator::mutate( double delta )
 {
   for ( auto& s : states )
   {
-    Distribution::mutate( s.operation_dist, gen );
-    Distribution::mutate( s.target_type_dist, gen );
-    Distribution::mutate( s.target_value_dist, gen );
-    Distribution::mutate( s.source_type_dist, gen );
-    Distribution::mutate( s.source_value_dist, gen );
-    Distribution::mutate( s.transition_dist, gen );
-    Distribution::mutate( s.position_dist, gen );
+    s.operation_dist = Distribution::mutate( s.operation_dist, gen );
+    s.target_type_dist = Distribution::mutate( s.target_type_dist, gen );
+    s.target_value_dist = Distribution::mutate( s.target_value_dist, gen );
+    s.source_type_dist = Distribution::mutate( s.source_type_dist, gen );
+    s.source_value_dist = Distribution::mutate( s.source_value_dist, gen );
+    s.transition_dist = Distribution::mutate( s.transition_dist, gen );
+    s.position_dist = Distribution::mutate( s.position_dist, gen );
   }
 }
 
--- src/generator.hpp
+++ src/generator.hpp
@@ -1,9 +1,28 @@
 #pragma once
 
-#include "distribution.hpp"
 #include "number.hpp"
 #include "program.hpp"
 #include "util.hpp"
+
+#include <random>
+
+class Distribution: public std::discrete_distribution<>
+{
+public:
+
+  Distribution( const std::vector<double>& probabilities );
+
+  static Distribution uniform( size_t size );
+
+  static Distribution exponential( size_t size );
+
+  static Distribution add( const Distribution& d1, const Distribution& d2 );
+
+  static Distribution mutate( const Distribution& d, std::mt19937& gen );
+
+  void print() const;
+
+};
 
 class State
 {
