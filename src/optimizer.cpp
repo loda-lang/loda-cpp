55d7ef47afee0fa332236b8b28214d5077c40901
Christian Krause <me@ckrause.org>
2020-03-07 18:22:15 +0100

    formatting

--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -304,84 +304,6 @@ bool Optimizer::simplifyOperations( Program &p, size_t num_initialized_cells ) c
   return simplified;
 }
 
-void Optimizer::minimize( Program &p, size_t num_terms ) const
-{
-  Log::get().debug( "Minimizing program" );
-  Interpreter interpreter( settings );
-  Sequence target_sequence;
-  try
-  {
-    target_sequence = interpreter.eval( p, num_terms );
-  }
-  catch ( const std::exception& )
-  {
-    return;
-  }
-  int removed_ops = 0;
-  for ( int i = 0; i < (int) p.ops.size(); ++i )
-  {
-    auto op = p.ops.at( i );
-    if ( op.type == Operation::Type::LPE )
-    {
-      continue;
-    }
-    else if ( op.type == Operation::Type::LPB )
-    {
-      if ( op.source.type != Operand::Type::CONSTANT || op.source.value != 1 )
-      {
-        p.ops.at( i ).source = Operand( Operand::Type::CONSTANT, 1 );
-        bool can_reset = true;
-        try
-        {
-          auto new_sequence = interpreter.eval( p, num_terms );
-          if ( new_sequence.size() != target_sequence.size() || new_sequence != target_sequence )
-          {
-            can_reset = false;
-          }
-        }
-        catch ( const std::exception& )
-        {
-          can_reset = false;
-        }
-        if ( !can_reset )
-        {
-          p.ops.at( i ) = op;
-        }
-      }
-    }
-    else
-    {
-      p.ops.erase( p.ops.begin() + i, p.ops.begin() + i + 1 );
-      bool can_remove = true;
-      try
-      {
-        auto new_sequence = interpreter.eval( p, num_terms );
-        if ( new_sequence.size() != target_sequence.size() || new_sequence != target_sequence )
-        {
-          can_remove = false;
-        }
-      }
-      catch ( const std::exception& )
-      {
-        can_remove = false;
-      }
-      if ( !can_remove )
-      {
-        p.ops.insert( p.ops.begin() + i, op );
-      }
-      else
-      {
-        --i;
-        ++removed_ops;
-      }
-    }
-  }
-  if ( removed_ops > 0 )
-  {
-    Log::get().debug( "Removed " + std::to_string( removed_ops ) + " operations during minimization" );
-  }
-}
-
 bool Optimizer::getUsedMemoryCells( const Program &p, std::unordered_set<number_t> &used_cells,
     number_t &largest_used ) const
 {
@@ -470,4 +392,82 @@ bool Optimizer::reduceMemoryCells( Program &p, size_t num_reserved_cells ) const
     }
   }
   return false;
+}
+
+void Optimizer::minimize( Program &p, size_t num_terms ) const
+{
+  Log::get().debug( "Minimizing program" );
+  Interpreter interpreter( settings );
+  Sequence target_sequence;
+  try
+  {
+    target_sequence = interpreter.eval( p, num_terms );
+  }
+  catch ( const std::exception& )
+  {
+    return;
+  }
+  int removed_ops = 0;
+  for ( int i = 0; i < (int) p.ops.size(); ++i )
+  {
+    auto op = p.ops.at( i );
+    if ( op.type == Operation::Type::LPE )
+    {
+      continue;
+    }
+    else if ( op.type == Operation::Type::LPB )
+    {
+      if ( op.source.type != Operand::Type::CONSTANT || op.source.value != 1 )
+      {
+        p.ops.at( i ).source = Operand( Operand::Type::CONSTANT, 1 );
+        bool can_reset = true;
+        try
+        {
+          auto new_sequence = interpreter.eval( p, num_terms );
+          if ( new_sequence.size() != target_sequence.size() || new_sequence != target_sequence )
+          {
+            can_reset = false;
+          }
+        }
+        catch ( const std::exception& )
+        {
+          can_reset = false;
+        }
+        if ( !can_reset )
+        {
+          p.ops.at( i ) = op;
+        }
+      }
+    }
+    else
+    {
+      p.ops.erase( p.ops.begin() + i, p.ops.begin() + i + 1 );
+      bool can_remove = true;
+      try
+      {
+        auto new_sequence = interpreter.eval( p, num_terms );
+        if ( new_sequence.size() != target_sequence.size() || new_sequence != target_sequence )
+        {
+          can_remove = false;
+        }
+      }
+      catch ( const std::exception& )
+      {
+        can_remove = false;
+      }
+      if ( !can_remove )
+      {
+        p.ops.insert( p.ops.begin() + i, op );
+      }
+      else
+      {
+        --i;
+        ++removed_ops;
+      }
+    }
+  }
+  if ( removed_ops > 0 )
+  {
+    Log::get().debug( "Removed " + std::to_string( removed_ops ) + " operations during minimization" );
+  }
 }
