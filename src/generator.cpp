68b3f300b016d13f5da2a2b8a4878b5645faf6a0
Christian Krause <me@ckrause.org>
2020-06-20 02:27:26 +0200

    updated 62 programs

--- src/generator.cpp
+++ src/generator.cpp
@@ -34,7 +34,7 @@ std::discrete_distribution<> operationDist( const std::vector<Operation::Type> &
       Log::get().error( "Unexpected stats for operation type: " + Operation::Metadata::get( operation_types[i] ).name,
           true );
     }
-    rate = std::min<int64_t>( rate / 1000, 1 );
+    rate = std::max<int64_t>( rate / 1000, 1 );
     p[i] = rate;
   }
   return std::discrete_distribution<>( p.begin(), p.end() );
