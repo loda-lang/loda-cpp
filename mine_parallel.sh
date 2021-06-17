e5e7b504aed8bcaf275441f7710583a99193cfdc
Christian Krause <me@ckrause.org>
2021-06-17 12:04:29 +0200

    updated 90/29847 programs

--- mine_parallel.sh
+++ mine_parallel.sh
@@ -36,6 +36,10 @@ fi
 
 # calculate minimum number of changes before commit
 min_changes=$(( $num_cpus * 2 ))
+min_changes=1000
+
+# increase metric publishing interval because we run multiple instances in parallel
+export LODA_METRICS_PUBLISH_INTERVAL=600
 
 function stop_miners() {
   echo "Stopping miners"
