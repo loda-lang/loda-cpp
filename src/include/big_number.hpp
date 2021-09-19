4ebcbfc7c4a019e9fefa2e51367655089ef3e292
Christian Krause <me@ckrause.org>
2021-09-19 21:52:16 +0200

    extend big int range to 48 words

--- src/include/big_number.hpp
+++ src/include/big_number.hpp
@@ -7,7 +7,7 @@
 
 class BigNumber {
  public:
-  static constexpr size_t NUM_WORDS = 40;
+  static constexpr size_t NUM_WORDS = 48;
   static constexpr size_t NUM_WORD_DIGITS = 18;
   static constexpr size_t NUM_DIGITS = NUM_WORDS * NUM_WORD_DIGITS;
   static constexpr int64_t WORD_BASE_ROOT = 1000000000;
