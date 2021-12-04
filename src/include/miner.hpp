1ee5835831a7f7fd51857d7fb370caa920b1915b
Christian Krause <me@ckrause.org>
2021-12-04 19:21:42 +0100

    improve-scheduling

--- src/include/miner.hpp
+++ src/include/miner.hpp
@@ -9,6 +9,7 @@
 #include "oeis_manager.hpp"
 #include "optimizer.hpp"
 #include "program.hpp"
+#include "setup.hpp"
 #include "util.hpp"
 
 class Miner {
@@ -28,6 +29,8 @@ class Miner {
   void submit(const std::string &id, const std::string &path);
 
  private:
+  void checkRegularTasks();
+
   void reload(bool load_generators, bool force_overwrite = false);
 
   void ensureSubmittedBy(Program &program);
@@ -35,8 +38,17 @@ class Miner {
   void setSubmittedBy(Program &program);
 
   static const std::string ANONYMOUS;
+  static const int64_t PROGRAMS_TO_FETCH;
   const Settings &settings;
+  const MiningMode mining_mode;
   std::unique_ptr<ApiClient> api_client;
   std::unique_ptr<OeisManager> manager;
   std::unique_ptr<MultiGenerator> multi_generator;
+  Generator *generator;
+  AdaptiveScheduler log_scheduler;
+  AdaptiveScheduler metrics_scheduler;
+  AdaptiveScheduler api_scheduler;
+  AdaptiveScheduler reload_scheduler;
+  int64_t generated_count;
+  int64_t current_fetch;
 };
