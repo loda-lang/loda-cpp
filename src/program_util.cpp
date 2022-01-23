54b07765f70d54617b48ae24c1bb553565285d6c
Christian Krause <me@ckrause.org>
2022-01-23 14:11:22 +0100

    fix

--- src/program_util.cpp
+++ src/program_util.cpp
@@ -107,7 +107,7 @@ bool ProgramUtil::isArithmetic(Operation::Type t) {
 bool ProgramUtil::isCommutative(Operation::Type t) {
   return (t == Operation::Type::ADD || t == Operation::Type::MUL ||
           t == Operation::Type::MIN || t == Operation::Type::MAX ||
-          t == Operation::Type::GCD);
+          t == Operation::Type::GCD || t == Operation::Type::CMP);
 }
 
 bool ProgramUtil::hasIndirectOperand(const Operation &op) {
