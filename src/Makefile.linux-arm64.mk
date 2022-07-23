50cd0efe32acc0886d987424fca00626b6c96461
Christian Krause <me@ckrause.org>
2022-07-23 20:33:42 +0200

    fix static linking of pthread library (#163)

--- src/Makefile.linux-arm64.mk
+++ src/Makefile.linux-arm64.mk
@@ -1,6 +1,8 @@
 # BEGIN PLATFORM CONFIG FOR LINUX ARM64
+# see https://stackoverflow.com/questions/35116327/when-g-static-link-pthread-cause-segmentation-fault-why
 CXX = aarch64-linux-gnu-g++
-LDFLAGS = -static -static-libstdc++ -static-libgcc
+CXXFLAGS += -pthread
+LDFLAGS = -static -static-libstdc++ -static-libgcc -lrt -pthread -Wl,--whole-archive -lpthread -Wl,--no-whole-archive
 # END PLATFORM CONFIG FOR LINUX ARM64
 
 include Makefile
--- src/Makefile.linux-x86.mk
+++ src/Makefile.linux-x86.mk
@@ -1,6 +1,8 @@
 # BEGIN PLATFORM CONFIG FOR LINUX X86
+# see https://stackoverflow.com/questions/35116327/when-g-static-link-pthread-cause-segmentation-fault-why
 CXX = x86_64-linux-gnu-g++
-LDFLAGS = -static -static-libstdc++ -static-libgcc
+CXXFLAGS += -pthread
+LDFLAGS = -static -static-libstdc++ -static-libgcc -lrt -pthread -Wl,--whole-archive -lpthread -Wl,--no-whole-archive
 # END PLATFORM CONFIG FOR LINUX X86
 
 include Makefile
