e5004fd39ebb028bcd52ac993dc027c26df92c31
Christian Krause <me@ckrause.org>
2022-09-16 21:59:02 +0200

    fix

--- src/include/process.hpp
+++ src/include/process.hpp
@@ -2,18 +2,23 @@
 
 #include <string>
 
+// must be before <psapi.h>
 #ifdef _WIN64
-
 #include <windows.h>
+#endif
 
-HANDLE createWindowsProcess(const std::string& command);
-
+#ifdef _WIN64
+#include <io.h>
+#include <psapi.h>
 #else
-
 #include <sys/wait.h>
 #include <unistd.h>
-typedef int64_t HANDLE;
+#endif
 
+#ifdef _WIN64
+HANDLE createWindowsProcess(const std::string& command);
+#else
+typedef int64_t HANDLE;
 #endif
 
 bool isChildProcessAlive(HANDLE pid);
