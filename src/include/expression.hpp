3d73b065c4b9c24a4063b092f6caf0b40ae0cb75
Christian Krause <me@ckrause.org>
2022-09-30 19:29:52 +0200

    expression class (#180)

---
+++ src/include/expression.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <vector>
+
+#include "number.hpp"
+
+class Expression {
+ public:
+  enum class Type {
+    CONSTANT,
+    PARAMETER,
+    FUNCTION,
+    SUM,
+    DIFFERENCE,
+    PRODUCT,
+    FRACTION
+  };
+
+  Expression();
+
+  ~Expression();
+
+  Expression(Type type, const std::string& name = "",
+             const Number& value = Number::ZERO);
+
+  Expression(const Expression& e);
+
+  Expression(Expression&& e);
+
+  Expression& operator=(const Expression& e);
+
+  Expression& operator=(Expression&& e);
+
+  bool operator==(const Expression& e);
+
+  bool operator!=(const Expression& e);
+
+  Expression& newChild(Type type, const std::string& name = "",
+                       const Number& value = Number::ZERO);
+
+  void replaceAll(const Expression& from, const Expression& to);
+
+  friend std::ostream& operator<<(std::ostream& out, const Expression& e);
+
+  std::string toString() const;
+
+  Type type;
+  std::string name;
+  Number value;
+  std::vector<Expression*> children;
+
+ private:
+  void printChildren(std::ostream& out, const std::string& op) const;
+
+  bool equalChildren(const Expression& e);
+};
