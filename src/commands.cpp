3e64e8ff39b6888083f8fa4e7858b1fe784ca27a
Christian Krause <me@ckrause.org>
2022-11-12 21:45:12 +0100

    fix test

--- src/commands.cpp
+++ src/commands.cpp
@@ -327,7 +327,7 @@ void Commands::testPari() {
     if (inceval.init(program) ||
         pariCode.find("binomial") != std::string::npos ||
         ProgramUtil::hasOp(program, Operation::Type::SEQ)) {
-      numTerms = std::min<size_t>(numTerms, 5);
+      numTerms = std::min<size_t>(numTerms, 4);
     }
     evaluator.eval(program, expSeq, numTerms);
     auto genSeq = Pari::eval(formula, 0, numTerms - 1);
