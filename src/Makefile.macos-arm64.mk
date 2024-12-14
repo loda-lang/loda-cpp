05edc224b345eb0dadbfe62c05eaa9d20805675b
Christian Krause <me@ckrause.org>
2024-12-14 12:32:26 +0100

    update to macos14 build

--- src/Makefile.macos-arm64.mk
+++ src/Makefile.macos-arm64.mk
@@ -1,7 +1,7 @@
 # BEGIN PLATFORM CONFIG FOR MAC OS ARM64
 CXX = clang++
-CXXFLAGS = -target arm64-apple-macos11
-LDFLAGS = -target arm64-apple-macos11
+CXXFLAGS = -target arm64-apple-macos14
+LDFLAGS = -target arm64-apple-macos14
 # END PLATFORM CONFIG FOR MAC OS ARM64
 
 include Makefile
--- src/Makefile.macos-x86.mk
+++ src/Makefile.macos-x86.mk
@@ -1,7 +1,7 @@
 # BEGIN PLATFORM CONFIG FOR MAC OS X86
 CXX = clang++
-CXXFLAGS = -target x86_64-apple-macos11
-LDFLAGS = -target x86_64-apple-macos11
+CXXFLAGS = -target x86_64-apple-macos14
+LDFLAGS = -target x86_64-apple-macos14
 # END PLATFORM CONFIG FOR MAC OS X86
 
 include Makefile
