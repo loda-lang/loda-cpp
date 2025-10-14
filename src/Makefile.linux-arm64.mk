4f36bb19d70f57a1ca480e769e0708e19c2392e5
Christian Krause <me@ckrause.org>
2025-10-14 19:22:54 +0200

    fix

--- src/Makefile.linux-arm64.mk
+++ src/Makefile.linux-arm64.mk
@@ -1,7 +1,6 @@
 # BEGIN PLATFORM CONFIG FOR LINUX ARM64
 # see https://stackoverflow.com/questions/35116327/when-g-static-link-pthread-cause-segmentation-fault-why
-# On native ARM64 systems, g++ is the ARM64 compiler
-CXX = g++
+CXX = aarch64-linux-gnu-g++
 CXXFLAGS += -pthread
 LDFLAGS = -static -static-libstdc++ -static-libgcc -lrt -pthread -Wl,--whole-archive -lpthread -Wl,--no-whole-archive -s
 # END PLATFORM CONFIG FOR LINUX ARM64
