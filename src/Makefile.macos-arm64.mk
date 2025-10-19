9479c5b62c9b9610df249af16f93c8aec41567eb
Maksim Chepurin <mchepurin@ozon.ru>
2025-10-19 12:12:36 +0500

    macos ARM64

--- src/Makefile.macos-arm64.mk
+++ src/Makefile.macos-arm64.mk
@@ -1,7 +1,11 @@
 # BEGIN PLATFORM CONFIG FOR MAC OS ARM64
 CXX = clang++
-CXXFLAGS = -target arm64-apple-macos14 -flto=thin
-LDFLAGS = -target arm64-apple-macos14 -flto=thin
+#CXXFLAGS = -target arm64-apple-macos14 -flto=thin -fprofile-generate
+#LDFLAGS = -target arm64-apple-macos14 -flto=thin -fprofile-generate
+CXXFLAGS = -target arm64-apple-macos14 -flto=thin -fprofile-use=../mine_1h.profdata -Wno-error=backend-plugin
+LDFLAGS = -target arm64-apple-macos14 -flto=thin -fprofile-use=../mine_1h.profdata
+#CXXFLAGS = -target arm64-apple-macos14 -flto=thin
+#LDFLAGS = -target arm64-apple-macos14 -flto=thin
 # END PLATFORM CONFIG FOR MAC OS ARM64
 
 include Makefile
