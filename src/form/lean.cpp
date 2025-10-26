124b14e01e1bc02f828a073afe71586470e3ee45
Christian Krause <me@ckrause.org>
2025-10-26 15:37:34 +0100

    simplify lean exprs

--- src/form/lean.cpp
+++ src/form/lean.cpp
@@ -273,6 +273,9 @@ void LeanFormula::transformParameterReferences(
     sum.children.push_back(ExpressionUtil::newConstant(offset));
     expr = sum;
   }
+
+  // finally simplify
+  ExpressionUtil::normalize(expr);
 }
 
 std::string LeanFormula::printEvalCode(int64_t offset, int64_t numTerms) const {
