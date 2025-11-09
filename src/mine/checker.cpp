4c49b58cc759bc9e645ad5445857ecf6d07be480
loader3229 <1010903229@qq.com>
2025-11-10 02:44:58 +0800

    commit

--- src/mine/checker.cpp
+++ src/mine/checker.cpp
@@ -48,15 +48,16 @@ bool hasIndirectOperand(const Program& p) {
 
 bool isSimpler(const Program& existing, const Program& optimized) {
   bool optimized_has_seq = ProgramUtil::hasOp(optimized, Operation::Type::SEQ);
+  bool existing_has_seq = ProgramUtil::hasOp(existing, Operation::Type::SEQ);
   if (hasBadConstant(existing) && !hasBadConstant(optimized) &&
       !optimized_has_seq) {
     return true;
-  } else if (!hasBadConstant(existing) && hasBadConstant(optimized)) {
+  } else if (!hasBadConstant(existing) && hasBadConstant(optimized) && !existing_has_seq) {
     return false;
   }
   if (hasBadLoop(existing) && !hasBadLoop(optimized) && !optimized_has_seq) {
     return true;
-  } else if (!hasBadLoop(existing) && hasBadLoop(optimized)) {
+  } else if (!hasBadLoop(existing) && hasBadLoop(optimized) && !existing_has_seq) {
     return false;
   }
   auto info_existing = Constants::findConstantLoop(existing);
@@ -64,7 +65,7 @@ bool isSimpler(const Program& existing, const Program& optimized) {
   if (info_existing.has_constant_loop && !info_optimized.has_constant_loop &&
       !optimized_has_seq) {
     return true;
-  }else if (!info_existing.has_constant_loop && info_optimized.has_constant_loop) {
+  }else if (!info_existing.has_constant_loop && info_optimized.has_constant_loop && !existing_has_seq) {
     return false;
   }
   if (hasIndirectOperand(existing) && !hasIndirectOperand(optimized)) {
