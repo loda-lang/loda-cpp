0c95485936434f76527786775e9efa644fb15748
Christian Krause <me@ckrause.org>
2025-11-22 14:01:42 +0100

    support url params in webclient

--- src/sys/web_client.hpp
+++ src/sys/web_client.hpp
@@ -5,13 +5,13 @@
 
 class WebClient {
  public:
-  static bool get(const std::string &url, const std::string &local_path,
+  static bool get(const std::string& url, const std::string& local_path,
                   bool silent = false, bool fail_on_error = true,
                   bool insecure = false);
 
-  static bool postFile(const std::string &url, const std::string &file_path,
-                       const std::string &auth = std::string(),
-                       const std::vector<std::string> &headers = {},
+  static bool postFile(const std::string& url, const std::string& file_path,
+                       const std::string& auth = std::string(),
+                       const std::vector<std::string>& headers = {},
                        bool enable_debug = false);
 
  private:
