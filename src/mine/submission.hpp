641de57929f89da5bf2ea129b1fda7336541f941
Christian Krause <me@ckrause.org>
2025-11-22 16:28:31 +0100

    add submission class

---
+++ src/mine/submission.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+
+#include "base/uid.hpp"
+#include "lang/program.hpp"
+#include "sys/jute.h"
+
+class Submission {
+ public:
+  enum class Type { PROGRAM, SEQUENCE };
+  enum class Mode { ADD, UPDATE, DELETE };
+
+  UID id;
+  Type type;
+  Mode mode;
+  std::string content;
+  std::string submitter;
+
+  static Submission fromJson(const jute::jValue& json);
+
+  Program toProgram() const;
+
+  static Mode modeFromString(const std::string& mode_str);
+  static Type typeFromString(const std::string& type_str);
+  static std::string modeToString(Mode mode);
+  static std::string typeToString(Type type);
+};
